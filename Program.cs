7719cff94d969a71369c14059fd4f212cf13187f
missAnkita <47353478+missAnkita@users.noreply.github.com>
2024-08-02 17:42:58 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,12 +2,9 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
-var host = new HostBuilder()
-    .ConfigureFunctionsWebApplication()
-    .ConfigureServices(services => {
-        services.AddApplicationInsightsTelemetryWorkerService();
-        services.ConfigureFunctionsApplicationInsights();
-    })
-    .Build();
+var host = new HostBuilder().ConfigureFunctionsWebApplication().ConfigureServices(services =>{
+  services.AddApplicationInsightsTelemetryWorkerService();
+  services.ConfigureFunctionsApplicationInsights();
+}).Build();
 
 host.Run();
